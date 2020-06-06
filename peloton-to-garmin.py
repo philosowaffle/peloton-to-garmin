1f849ebe1d7676aaf54f17ee333e3b7ea1c41758
Nate Everden <nate@macbook.planetfire.com>
2020-06-06 17:18:26 -0400

    Added command line parser.

--- peloton-to-garmin.py
+++ peloton-to-garmin.py
@@ -7,6 +7,7 @@ import os
 import sys
 import json
 import logging
+import argparse
 
 from lib import pelotonApi
 from lib import config_helper as config
@@ -21,16 +22,29 @@ else:
     pause_on_finish = config.ConfigSectionMap("DEBUG")['pauseonfinish']
 
 ##############################
+# Command Line Argument Setup 
+##############################
+argParser = argparse.ArgumentParser()
+
+argParser.add_argument("-email",help="Peloton email address",dest="email",type=str)
+argParser.add_argument("-password",help="Peloton password",dest="password",type=str)
+argParser.add_argument("-path",help="Path to output directory",dest="output_dir",type=str)
+argParser.add_argument("-num",help="Number of activities to download",dest="num_to_download",type=int)
+argParser.add_argument("-log",help="Log file name",dest="log_file",type=int)
+
+argResults = argParser.parse_args()
+
+##############################
 # Logging Setup
 ##############################
-if len(sys.argv) > 3:
-    file_handler = logging.FileHandler(sys.argv[3])
-else:
-    if config.ConfigSectionMap("LOGGER")['logfile'] is None:
-        logger.error("Please specify a path for the logfile.")
-        sys.exit(1)
+if argResults.log_file is not None:
+    file_handler = logging.FileHandler(argResults.log_file)
+elif config.ConfigSectionMap("LOGGER")['logfile'] is not None:
     file_handler = logging.FileHandler(config.ConfigSectionMap("LOGGER")['logfile'])
-
+else:
+    logger.error("Please specify a path for the logfile.")
+    sys.exit(1)
+    
 logger = logging.getLogger('peloton-to-garmin')
 # logger.setLevel(logging.DEBUG)
 logger.setLevel(logging.INFO)
@@ -53,14 +67,21 @@ logger.addHandler(console_handler)
 logger.debug("Peloton to Garmin Magic :)")
 
 ##############################
-# Environment Variables Setup
+# Variables Setup
 ##############################
-if os.getenv("NUM_ACTIVITIES") is not None:
-    numActivities = os.getenv("NUM_ACTIVITIES")
-else:
-    numActivities = None
+user_email = None
+user_password = None
+output_directory = None
+numActivities = None
 
-if os.getenv("OUTPUT_DIRECTORY") is not None:
+if argResults.num_to_download is not None:
+    numActivities = argResults.num_to_download
+elif os.getenv("NUM_ACTIVITIES") is not None:
+    numActivities = os.getenv("NUM_ACTIVITIES")
+
+if argResults.output_dir is not None:
+    output_directory = argResults.output_dir
+elif os.getenv("OUTPUT_DIRECTORY") is not None:
     output_directory = os.getenv("OUTPUT_DIRECTORY")
 else:
     output_directory = config.ConfigSectionMap("OUTPUT")['directory']
@@ -68,21 +89,21 @@ else:
 ##############################
 # Peloton Setup
 ##############################
-
-if len(sys.argv) > 2:
-    user_email = sys.argv[1]
-    user_password = sys.argv[2]
-else :
-    if config.ConfigSectionMap("PELOTON")['email'] is None:
-        logger.error("Please specify your Peloton login email in the config.ini file.")
-        sys.exit(1)
-
-    if config.ConfigSectionMap("PELOTON")['password'] is None:
-        logger.error("Please specify your Peloton login password in the config.ini file.")
-        sys.exit(1)
-
+if argResults.email is not None:
+    user_email = argResults.email
+elif config.ConfigSectionMap("PELOTON")['email'] is not None:
     user_email = config.ConfigSectionMap("PELOTON")['email']
+else:
+    logger.error("Please specify your Peloton login email in the config.ini file.")
+    sys.exit(1)
+
+if argResults.password is not None:
+    user_password = argResults.password
+elif config.ConfigSectionMap("PELOTON")['password'] is not None:
     user_password = config.ConfigSectionMap("PELOTON")['password']
+else:
+    logger.error("Please specify your Peloton login password in the config.ini file.")
+    sys.exit(1)
 
 api = pelotonApi.PelotonApi(user_email, user_password)
 
