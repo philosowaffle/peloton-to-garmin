dfe6d52f730e2455435d8c1db4de660ac682654e
Bailey Belvis <baileyb622@gmail.com>
2021-03-06 13:28:53 -0600

    planning out TODOs

---
+++ PelotonToFit/PelotonToFitConsole/ProcessedHistoryItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PelotonToFitConsole
+{
+	public class ProcessedHistoryItem
+	{
+		public string WorkoutTitle { get; set; }
+		public DateTime ProcessedDate { get; set; }
+		public DateTime WorkoutDate { get; set; }
+		public string WorkoutId { get; set; }
+	}
+}
