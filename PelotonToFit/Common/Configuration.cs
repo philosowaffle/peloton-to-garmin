36a16081f2e2d53a495a72d52e21a2d71074d38e
Bailey <baileyb622@gmail.com>
2021-03-07 13:13:53 -0600

    I think I almost have feature parity with the existing application

---
+++ PelotonToFit/Common/Configuration.cs
@@ -0,0 +1,114 @@
+﻿using System.Text.Json;
+using System;
+using System.IO;
+using System.Text.Json.Serialization;
+
+namespace Common
+{
+	public static class ConfigurationLoader
+	{
+		public static bool TryLoadConfigurationFile(out Configuration config)
+		{
+			config = new Configuration();
+
+			var configFilePath = Path.Join(Environment.CurrentDirectory, "configuration.local.json");
+			if (!File.Exists(configFilePath))
+			{
+				Console.Out.WriteLine($"Failed to find file: {configFilePath}");
+				Console.Out.WriteLine($"Creating file: {configFilePath}");
+				Console.Out.WriteLine();
+				Console.Out.WriteLine("***Please modify the file listed above with your configuration values and run the application again.***");
+				Console.Out.WriteLine();
+				File.WriteAllText(configFilePath, JsonSerializer.Serialize(config, new JsonSerializerOptions() { WriteIndented = true }));
+				return false;
+			}
+
+			try
+			{
+				using (var reader = new StreamReader(configFilePath))
+				{
+					config = JsonSerializer.Deserialize<Configuration>(reader.ReadToEnd(), new JsonSerializerOptions() { ReadCommentHandling = JsonCommentHandling.Skip });
+				}
+				
+				// write data back to the file so that any newly added config values exist for the future
+				if (config.DoNotEdit_ConfigVersion != Configuration.CurrentConfigVersion)
+				{
+					config.DoNotEdit_ConfigVersion = Configuration.CurrentConfigVersion;
+					File.WriteAllText(configFilePath, JsonSerializer.Serialize(config, new JsonSerializerOptions() { WriteIndented = true }));
+				}
+
+				return true;
+			}
+			catch (Exception e)
+			{
+				Console.Out.WriteLine($"Failed to parse: {configFilePath}");
+				Console.Out.WriteLine($"Exception is: {e.Message}");
+				Console.Out.Write(e);
+				return false;
+			}
+		}
+	}
+
+	public class Configuration
+	{
+		[JsonIgnore]
+		public static readonly int CurrentConfigVersion = 1;
+
+		public Configuration()
+		{
+			Application = new ApplicationConfig();
+			Peloton = new PelotonConfig();
+			Garmin = new GarminConfig();
+			_configVersion = 0;
+		}
+
+		public ApplicationConfig Application { get; set; }
+		public PelotonConfig Peloton { get; set; }
+		public GarminConfig Garmin { get; set; }
+
+		private int? _configVersion = null;
+		public int DoNotEdit_ConfigVersion 
+		{
+			get { return _configVersion ?? CurrentConfigVersion; }
+			set { _configVersion = value; }
+		}
+	}
+
+	public class ApplicationConfig
+	{
+		/// <summary>
+		/// Test comment.
+		/// </summary>
+		public Severity DebugSeverity { get; set; }
+		public string OutputDirectory { get; set; }
+		public string ProcessedHistoryFilePath { get; set; }
+		public bool EnablePolling { get; set; }
+		public int PollingIntervalSeconds { get; set; }
+		public string PathToPythonExe { get; set; }
+
+		[JsonIgnore]
+		public string FitDirectory => Path.Join(OutputDirectory, "fit");
+	}
+
+	public class PelotonConfig
+	{
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public int NumWorkoutsToDownload { get; set; }
+	}
+
+	public class GarminConfig
+	{
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public bool Upload { get; set; }
+		public string PathToGarminUploadPy { get; set; }
+	}
+
+	public enum Severity
+	{
+		None,
+		Info,
+		Debug
+	}
+}
--- PelotonToFit/Garmin/GarminUploader.cs
+++ PelotonToFit/Garmin/GarminUploader.cs
@@ -1,8 +1,10 @@
 ﻿
+using Common;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace Garmin
 {
@@ -11,13 +13,24 @@ namespace Garmin
 		private static string[] ScriptPathParts = new string[] { Environment.CurrentDirectory, "..", "..", "..", "..", "..", "garminUpload.py" };
 		private static string ScriptPath = Path.Combine(ScriptPathParts);
 
-		public static bool UploadToGarmin(ICollection<string> filePaths, string garminEmail, string garminPassword, string pathToPythonExe)
+		public static bool UploadToGarmin(ICollection<string> filePaths, Configuration config)
 		{
-			ProcessStartInfo start = new ProcessStartInfo();
-			start.FileName = pathToPythonExe;
+			var uploadScriptPath = config.Garmin.PathToGarminUploadPy ?? ScriptPath;
+			var pythonPath = config.Application.PathToPythonExe;
 
-			var paths = String.Join(" -f ", filePaths);
-			var cmd = $"{ScriptPath} -ge {garminEmail} -gp {garminPassword} -f {paths}";
+			ProcessStartInfo start = new ProcessStartInfo();
+			start.FileName = pythonPath;
+
+			var paths = String.Join(" ", filePaths.Select(p => $"\"{p}\""));
+			var cmd = $"{uploadScriptPath} -ge {config.Garmin.Email} -gp {config.Garmin.Password} -f {paths}";
+
+			if (config.Application.DebugSeverity == Severity.Debug)
+			{
+				Console.WriteLine("Uploading to Garmin with the following parameters:");
+				Console.WriteLine($"Python: {pythonPath}");
+				Console.WriteLine($"File Paths: {paths}");
+				Console.WriteLine($"Full command: {cmd.Replace(config.Garmin.Email, "**email**").Replace(config.Garmin.Password, "**password**")}");
+			}
 			
 			start.Arguments = cmd;
 			start.UseShellExecute = false;
@@ -31,7 +44,8 @@ namespace Garmin
 					string stderr = process.StandardError.ReadToEnd();
 					string result = reader.ReadToEnd();
 
-					Console.Out.WriteLine(result);
+					if (config.Application.DebugSeverity == Severity.Debug)
+						Console.Out.WriteLine(result);
 
 					if (!string.IsNullOrEmpty(stderr))
 					{
@@ -40,6 +54,39 @@ namespace Garmin
 					}
 				}
 			}
+			return true;
+		}
+
+		public static bool ValidateConfig(Configuration config)
+		{
+			if (config.Garmin.Upload == false) return true;
+
+			var uploadScriptPath = config.Garmin.PathToGarminUploadPy ?? ScriptPath;
+			if (!File.Exists(uploadScriptPath))
+			{
+				Console.Out.WriteLine($"File does not exist, check your configuration {nameof(config.Garmin)}.{nameof(config.Garmin.PathToGarminUploadPy)} is correct: {uploadScriptPath}");
+				return false;
+			}
+
+			var pythonPath = config.Application.PathToPythonExe;
+			if (!File.Exists(pythonPath))
+			{
+				Console.Out.WriteLine($"File does not exist, check your configuration {nameof(config.Application)}.{nameof(config.Application.PathToPythonExe)} is correct: {pythonPath}");
+				return false;
+			}
+
+			if (string.IsNullOrEmpty(config.Garmin.Email))
+			{
+				Console.Out.WriteLine($"Garmin Email required, check your configuration {nameof(config.Garmin)}.{nameof(config.Garmin.Email)} is set.");
+				return false;
+			}
+
+			if (string.IsNullOrEmpty(config.Garmin.Password))
+			{
+				Console.Out.WriteLine($"Garmin Password required, check your configuration {nameof(config.Garmin)}.{nameof(config.Garmin.Password)} is set.");
+				return false;
+			}
+
 			return true;
 		}
 	}
