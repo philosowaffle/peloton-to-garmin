ec9c46a02dc37b4ccca221201a9ee6714c2b2f58
Bailey <baileyb622@gmail.com>
2021-03-14 13:01:45 -0500

    add some general processing metrics

--- PelotonToFit/Common/Metrics.cs
+++ PelotonToFit/Common/Metrics.cs
@@ -8,6 +8,11 @@ namespace Common
 	public static class Metrics
 	{
 		public static readonly Counter PollsCounter = PromMetrics.CreateCounter("polls_total", "The number of times the current process has polled for new data.");
+		public static readonly Histogram WorkoutConversionDuration = PromMetrics.CreateHistogram("workout_conversion_duration_seconds", "Histogram of workout conversion durations.", new HistogramConfiguration() 
+		{
+			LabelNames = new[] { "format" }
+		});
+
 
 		public static readonly Counter HttpResponseCounter = PromMetrics.CreateCounter("http_responses", "The number of http responses.", new CounterConfiguration
 		{
@@ -19,22 +24,7 @@ namespace Common
 			LabelNames = new[] { "method", "uri", "status_code", "duration_in_seconds", "message" }
 		});
 
-		public static IMetricServer EnableMetricsServer(Configuration config)
-		{
-			IMetricServer metricsServer = null;
-			if (config.Observability.Prometheus.Enabled)
-			{
-				var port = config.Observability.Prometheus.Port ?? 4000;
-				var registry = new CollectorRegistry();
-				var staticLabels = new Dictionary<string, string>() { { "app", "p2g" } };
-				registry.SetStaticLabels(staticLabels);
-				metricsServer = new KestrelMetricServer(port: port, registry: registry);
-				metricsServer.Start();
-				Console.Out.WriteLine($"Metrics Server started and listening on: http://localhost:{port}/metrics");
-			}
-
-			return metricsServer;
-		}
+		public static readonly Gauge WorkoutsToConvert = PromMetrics.CreateGauge("workout_conversion_pending", "The number of workouts pending conversion to output format.");
 
 		public static bool ValidateConfig(ObservabilityConfig config)
 		{
