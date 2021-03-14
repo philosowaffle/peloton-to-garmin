5f96e1cbd1d2af90bbe3bf29c58a1c10939a2a23
Bailey <baileyb622@gmail.com>
2021-03-14 12:42:52 -0500

    add http metrics

--- PelotonToFit/Common/Metrics.cs
+++ PelotonToFit/Common/Metrics.cs
@@ -1,5 +1,6 @@
 ﻿using Prometheus;
 using System;
+using System.Collections.Generic;
 using PromMetrics = Prometheus.Metrics;
 
 namespace Common
@@ -7,6 +8,33 @@ namespace Common
 	public static class Metrics
 	{
 		public static readonly Counter PollsCounter = PromMetrics.CreateCounter("polls_total", "The number of times the current process has polled for new data.");
+
+		public static readonly Counter HttpResponseCounter = PromMetrics.CreateCounter("http_responses", "The number of http responses.", new CounterConfiguration
+		{
+			LabelNames = new[] { "method", "uri", "status_code", "duration_in_seconds" }
+		});
+
+		public static readonly Counter HttpErrorCounter = PromMetrics.CreateCounter("http_errors", "The number of errors encountered.", new CounterConfiguration
+		{
+			LabelNames = new[] { "method", "uri", "status_code", "duration_in_seconds", "message" }
+		});
+
+		public static IMetricServer EnableMetricsServer(Configuration config)
+		{
+			IMetricServer metricsServer = null;
+			if (config.Observability.Prometheus.Enabled)
+			{
+				var port = config.Observability.Prometheus.Port ?? 4000;
+				var registry = new CollectorRegistry();
+				var staticLabels = new Dictionary<string, string>() { { "app", "p2g" } };
+				registry.SetStaticLabels(staticLabels);
+				metricsServer = new KestrelMetricServer(port: port, registry: registry);
+				metricsServer.Start();
+				Console.Out.WriteLine($"Metrics Server started and listening on: http://localhost:{port}/metrics");
+			}
+
+			return metricsServer;
+		}
 
 		public static bool ValidateConfig(ObservabilityConfig config)
 		{
