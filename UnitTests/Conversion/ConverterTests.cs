44b4b87018688172de283c7bfbe797cbc3eea0f9
bailey.belvis <bailey.belvis@match.com>
2021-03-24 16:27:20 -0500

    more cleanup

---
+++ UnitTests/Conversion/ConverterTests.cs
@@ -0,0 +1,21 @@
+﻿using Common.Dto;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Conversion
+{
+	public class ConverterTests
+	{
+		[Test]
+		public void GetStartTime()
+		{
+			var origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+			//return origin.AddSeconds(timestamp);
+			Assert.Pass();
+		}
+	}
+}
