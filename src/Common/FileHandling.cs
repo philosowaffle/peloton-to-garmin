30a763a0ef977d771ca95e3201ed4e641a8e934e
Bailey <baileyb622@gmail.com>
2021-05-31 08:29:37 -0500

    add tracing to file handling

--- src/Common/FileHandling.cs
+++ src/Common/FileHandling.cs
@@ -21,8 +21,10 @@ namespace Common
 	{
 		public void MkDirIfNotExists(string path)
 		{
-			if (!Directory.Exists(path))
+			using var trace1 = Tracing.Trace(nameof(MkDirIfNotExists), "io");
+			if (!DirExists(path))
 			{
+				using var trace2 = Tracing.Trace("CreateDirectory", "io");
 				Log.Debug("Creating directory {@Directory}", path);
 				Directory.CreateDirectory(path);
 			}
@@ -30,16 +32,19 @@ namespace Common
 
 		public bool DirExists(string path)
 		{
+			using var trace1 = Tracing.Trace(nameof(DirExists), "io");
 			return Directory.Exists(path);
 		}
 
 		public string[] GetFiles(string path)
 		{
+			using var trace1 = Tracing.Trace(nameof(GetFiles), "io");
 			return Directory.GetFiles(path);
 		}
 
 		public T DeserializeJson<T>(string file)
 		{
+			using var trace1 = Tracing.Trace(nameof(DeserializeJson), "io");
 			using (var reader = new StreamReader(file))
 			{
 				return JsonSerializer.Deserialize<T>(reader.ReadToEnd(), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
@@ -48,6 +53,7 @@ namespace Common
 
 		public void MoveFailedFile(string fromPath, string toPath)
 		{
+			using var trace1 = Tracing.Trace(nameof(MoveFailedFile), "io");
 			try
 			{
 				MkDirIfNotExists(toPath);
@@ -64,16 +70,19 @@ namespace Common
 
 		public void Copy(string from, string to, bool overwrite)
 		{
+			using var trace1 = Tracing.Trace(nameof(Copy), "io");
 			File.Copy(from, to, overwrite);
 		}
 
 		public void Cleanup(string dir)
 		{
-			if (!Directory.Exists(dir))
+			using var trace1 = Tracing.Trace(nameof(Cleanup), "io");
+			if (!DirExists(dir))
 				return;
 
 			try
 			{
+				using var trace2 = Tracing.Trace("DeleteDir", "io");
 				Log.Debug("Deleting directory.");
 				Directory.Delete(dir, recursive: true);
 			}
