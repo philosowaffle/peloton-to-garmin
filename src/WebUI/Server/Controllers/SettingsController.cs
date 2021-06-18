06efe31fd319e3b264b75741088a93665050b26a
Bailey Belvis <baileyb622@gmail.com>
2021-06-18 14:59:11 -0500

    print some of the settings on the UI

---
+++ src/WebUI/Server/Controllers/SettingsController.cs
@@ -0,0 +1,29 @@
+﻿using Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Server.Controllers
+{
+	[ApiController]
+	[Route("/settings")]
+	public class SettingsController : Controller
+	{
+		private IAppConfiguration _config;
+
+		public SettingsController(IAppConfiguration config)
+		{
+			_config = config;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			_config.Peloton.Email = "******" + _config.Peloton.Email.Substring(6);
+			_config.Peloton.Password = "******";
+
+			_config.Garmin.Email = "******" + _config.Peloton.Email.Substring(6);
+			_config.Garmin.Password = "******";
+
+			return Ok(_config);
+		}
+	}
+}
---
+++ src/WebUI/Shared/SettingsRequest.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebUI.Shared
+{
+	public class SettingsGetResponse : Configuration
+	{
+	}
+}
