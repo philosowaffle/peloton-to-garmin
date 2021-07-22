d6da1772b7116d6574492ca8499ba3933b38df3e
Bailey Belvis <baileyb622@gmail.com>
2021-07-21 20:05:47 -0500

    [122] add about page, fix some icons

---
+++ src/WebUI/Server/Controllers/SystemInfoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using WebUI.Shared;
+
+namespace WebUI.Server.Controllers
+{
+	[ApiController]
+	[Route("/api/systeminfo")]
+	public class SystemInfoController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			var systemInfo = new SystemInfoGetResponse()
+			{
+				OperatingSystem = Environment.OSVersion.Platform.ToString(),
+				OperatingSystemVersion = Environment.OSVersion.VersionString,
+
+				RunTimeVersion = Environment.Version.ToString(),
+
+				Version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion,
+
+				Website = "https://github.com/philosowaffle/peloton-to-garmin",
+				Documentation = "https://github.com/philosowaffle/peloton-to-garmin/wiki/Peloton-To-Garmin---v2",
+				Forums = "https://github.com/philosowaffle/peloton-to-garmin/discussions",
+				Donate = "https://www.buymeacoffee.com/philosowaffle",
+				Issues = "https://github.com/philosowaffle/peloton-to-garmin/issues"
+
+			};
+
+			return Ok(systemInfo);
+		}
+	}
+}
---
+++ src/WebUI/Shared/SystemInfoRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace WebUI.Shared
+{
+	public class SystemInfoGetResponse
+	{
+		public string RunTimeVersion { get; set; }
+		public string OperatingSystem { get; set; }
+		public string OperatingSystemVersion { get; set; }
+		public string Version { get; set; }
+		public string Website { get; set; }
+		public string Documentation { get; set; }
+		public string Forums { get; set; }
+		public string Donate { get; set; }
+		public string Issues { get; set; }
+	}
+}
