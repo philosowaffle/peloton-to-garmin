ad60f971cad7b84b5b9c0eae5368df88e22cf7b7
Bailey Belvis <baileyb622@gmail.com>
2021-10-30 19:21:04 -0500

    init MVC project

---
+++ src/WebApp/Program.cs
@@ -0,0 +1,51 @@
+using Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Enrichers.Span;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Services;
+
+namespace WebApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureAppConfiguration(configBuilder =>
+				{
+					configBuilder.AddJsonFile(Path.Join(Environment.CurrentDirectory, "configuration.local.json"), optional: true, reloadOnChange: true)
+					.AddEnvironmentVariables(prefix: "P2G_")
+					.AddCommandLine(args)
+					.Build();
+
+				})
+				.UseSerilog((ctx, logConfig) =>
+				{
+					logConfig
+					.ReadFrom.Configuration(ctx.Configuration, sectionName: $"{nameof(Observability)}:Serilog")
+					.Enrich.WithSpan()
+					.Enrich.FromLogContext();
+				})
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				})
+				.ConfigureServices(services =>
+				{
+					services.AddHostedService<SyncService>();
+				});
+	}
+}
