d4a0e8c4cbb1bd7669b1904b93f4cf6ebe47a125
Bailey <baileyb622@gmail.com>
2022-03-26 09:26:03 -0500

    entrypoint update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 chown -R p2g:p2g /app
 
 if [[ "$1" == "web" ]]; then
-    exec runuser -u p2g ./WebUI
+    # exec runuser -u p2g ./WebUI
 elif [[ "$1" == "api" ]]; then
     exec runuser -u p2g ./Api
 else
