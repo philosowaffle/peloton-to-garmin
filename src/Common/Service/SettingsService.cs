3995e7442ee8dfc0ce875104f87fd9e6d893b085
philosowaffle <baileyb622@gmail.com>
2022-07-30 11:47:25 -0500

    [273] webui - settings page add missing settings (#275)

--- src/Common/Service/SettingsService.cs
+++ src/Common/Service/SettingsService.cs
@@ -3,45 +3,44 @@ using Common.Observe;
 using Serilog;
 using System.Threading.Tasks;
 
-namespace Common.Service
+namespace Common.Service;
+
+public interface ISettingsService
 {
-    public interface ISettingsService
-    {
-        Task<Settings> GetSettingsAsync();
-        Task UpdateSettings(Settings settings);
-    }
+	Task<Settings> GetSettingsAsync();
+	Task UpdateSettings(Settings settings);
+}
 
-    public class SettingsService : ISettingsService
-    {
-        private static readonly ILogger _logger = LogContext.ForClass<SettingsService>();
+public class SettingsService : ISettingsService
+{
+	private static readonly ILogger _logger = LogContext.ForClass<SettingsService>();
 
-        private readonly ISettingsDb _db;
+	private readonly ISettingsDb _db;
 
-        public SettingsService(ISettingsDb db)
-        {
-            _db = db;
-        }
+	public SettingsService(ISettingsDb db)
+	{
+		_db = db;
+	}
 
-        public Task<Settings> GetSettingsAsync()
-        {
-            using var tracing = Tracing.Trace($"{nameof(SettingsService)}.{nameof(GetSettingsAsync)}");
+	public Task<Settings> GetSettingsAsync()
+	{
+		using var tracing = Tracing.Trace($"{nameof(SettingsService)}.{nameof(GetSettingsAsync)}");
 
-            return _db.GetSettingsAsync();
-        }
+		return _db.GetSettingsAsync();
+	}
 
-        public async Task UpdateSettings(Settings updatedSettings)
-        {
-            using var tracing = Tracing.Trace($"{nameof(SettingsService)}.{nameof(UpdateSettings)}");
+	public async Task UpdateSettings(Settings updatedSettings)
+	{
+		using var tracing = Tracing.Trace($"{nameof(SettingsService)}.{nameof(UpdateSettings)}");
 
-            var originalSettings = await _db.GetSettingsAsync();
+		var originalSettings = await _db.GetSettingsAsync();
 
-            if (updatedSettings.Garmin.Password is null)
-                updatedSettings.Garmin.Password = originalSettings.Garmin.Password;
+		if (updatedSettings.Garmin.Password is null)
+			updatedSettings.Garmin.Password = originalSettings.Garmin.Password;
 
-            if (updatedSettings.Peloton.Password is null)
-                updatedSettings.Peloton.Password = originalSettings.Peloton.Password;
+		if (updatedSettings.Peloton.Password is null)
+			updatedSettings.Peloton.Password = originalSettings.Peloton.Password;
 
-            await _db.UpsertSettingsAsync(updatedSettings);
-        }
-    }
+		await _db.UpsertSettingsAsync(updatedSettings);
+	}
 }
