368a4476069a408527ecf5556f5b0abe28f76fa4
philosowaffle <baileyb622@gmail.com>
2022-08-29 20:23:31 -0500

    set release version to 3.0.3

--- src/Common/Constants.cs
+++ src/Common/Constants.cs
@@ -8,6 +8,6 @@
 		public const string ApiName = "p2g_api";
 		public const string WebUIName = "p2g_webui";
 
-		public const string AppVersion = "3.0.3-rc";
+		public const string AppVersion = "3.0.3";
 	}
 }
