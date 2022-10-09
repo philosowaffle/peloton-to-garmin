698c07b5cf3cf65a2c8ec60726ed0c0d60e7d2a8
Bailey <baileyb622@gmail.com>
2022-10-09 08:57:58 -0500

    unit tests

---
+++ src/UnitTests/Api/Controllers/SystemInfoControllerTests.cs
@@ -0,0 +1,87 @@
+﻿using Common.Dto.Api;
+using FluentAssertions;
+using GitHub;
+using GitHub.Dto;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Moq.AutoMock;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using WebApp.Controllers;
+
+namespace UnitTests.Api.Controllers;
+
+public class SystemInfoControllerTests
+{
+	[Test]
+	public async Task GetAsync_WhenNotRequest_DoesNotEnrich_LatestVersionInfo()
+	{
+		// SETUP
+		var autoMocker = new AutoMocker();
+		var controller = autoMocker.CreateInstance<SystemInfoController>();
+		var ghService = autoMocker.GetMock<IGitHubService>();
+
+		var context = new DefaultHttpContext();
+		context.Request.Scheme = "https";
+		context.Request.Host = new HostString("localhost");
+		controller.ControllerContext = new Microsoft.AspNetCore.Mvc.ControllerContext()
+		{
+			HttpContext = context
+		};
+
+		var request = new SystemInfoGetRequest() { CheckForUpdate = false };
+
+		// ACT
+		var actionResult = await controller.GetAsync(request);
+
+		// ASSERT
+		var response = actionResult.Value;
+		response.Should().NotBeNull();
+
+		response.NewerVersionAvailable.Should().BeNull();
+		response.LatestVersionInformation.Should().BeNull();
+
+		ghService.Verify(x => x.GetLatestReleaseAsync(), Times.Never());
+	}
+
+	[Test]
+	public async Task GetAsync_WhenRequest_Enriches_LatestVersionInfo()
+	{
+		// SETUP
+		var autoMocker = new AutoMocker();
+		var controller = autoMocker.CreateInstance<SystemInfoController>();
+		var ghService = autoMocker.GetMock<IGitHubService>();
+
+		var context = new DefaultHttpContext();
+		context.Request.Scheme = "https";
+		context.Request.Host = new HostString("localhost");
+		controller.ControllerContext = new Microsoft.AspNetCore.Mvc.ControllerContext()
+		{
+			HttpContext = context
+		};
+
+		ghService.Setup(x => x.GetLatestReleaseAsync())
+			.ReturnsAsync(new P2GLatestRelease()
+			{
+				IsReleaseNewerThanInstalledVersion = true, 
+				Description = "adf",
+				LatestVersion = "v45.90.23",
+				ReleaseDate = DateTime.Now,
+				ReleaseUrl = "https://www.google.com"
+			});
+
+		// ACT
+		var request = new SystemInfoGetRequest() { CheckForUpdate = true };
+		var actionResult = await controller.GetAsync(request);
+
+		// ASSERT
+		var response = actionResult.Value;
+		response.Should().NotBeNull();
+
+		response.NewerVersionAvailable.Should().NotBeNull();
+		response.LatestVersionInformation.Should().NotBeNull();
+
+		ghService.Verify(x => x.GetLatestReleaseAsync(), Times.Once());
+	}
+}
--- src/UnitTests/Sync/SyncServiceTests.cs
+++ src/UnitTests/Sync/SyncServiceTests.cs
@@ -6,6 +6,8 @@ using Common.Service;
 using Conversion;
 using FluentAssertions;
 using Garmin;
+using GitHub;
+using GitHub.Dto;
 using Moq;
 using Moq.AutoMock;
 using NUnit.Framework;
@@ -32,6 +34,7 @@ namespace UnitTests.Sync
 			var settingsService = mocker.GetMock<ISettingsService>();
 			var settings = new Settings();
 			settings.Format.Fit = true;
+			settings.App.CheckForUpdates = false;
 			settingsService.Setup(s => s.GetSettingsAsync()).ReturnsAsync(settings);
 
 			var syncStatus = new SyncServiceStatus();
@@ -64,6 +67,7 @@ namespace UnitTests.Sync
 			var settingsService = mocker.GetMock<ISettingsService>();
 			var settings = new Settings();
 			settings.Format.Fit = true;
+			settings.App.CheckForUpdates = false;
 			settingsService.Setup(s => s.GetSettingsAsync()).ReturnsAsync(settings);
 
 			var syncStatus = new SyncServiceStatus();
@@ -101,6 +105,7 @@ namespace UnitTests.Sync
 
 			var settings = new Settings();
 			settings.Format.Fit = true;
+			settings.App.CheckForUpdates = false;
 			settingsService.Setup(s => s.GetSettingsAsync()).ReturnsAsync(settings);
 
 			converter.Setup(c => c.ConvertAsync(It.IsAny<P2GWorkout>())).ReturnsAsync(new ConvertStatus() { Result = ConversionResult.Success });
@@ -143,6 +148,7 @@ namespace UnitTests.Sync
 
 			var settings = new Settings();
 			settings.Format.Fit = true;
+			settings.App.CheckForUpdates = false;
 			settingsService.Setup(s => s.GetSettingsAsync()).ReturnsAsync(settings);
 
 			converter.Setup(c => c.ConvertAsync(It.IsAny<P2GWorkout>())).ReturnsAsync(new ConvertStatus() { Result = ConversionResult.Success });
@@ -185,6 +191,7 @@ namespace UnitTests.Sync
 
 			var settings = new Settings();
 			settings.Format.Fit = true;
+			settings.App.CheckForUpdates = false;
 			settingsService.Setup(s => s.GetSettingsAsync()).ReturnsAsync(settings);
 
 			converter.Setup(c => c.ConvertAsync(It.IsAny<P2GWorkout>())).ReturnsAsync(new ConvertStatus() { Result = ConversionResult.Success });
@@ -216,6 +223,67 @@ namespace UnitTests.Sync
 			garmin.Verify(x => x.UploadToGarminAsync(), Times.Once);
 			db.Verify(x => x.UpsertSyncStatusAsync(It.IsAny<SyncServiceStatus>()), Times.Once);
 			fileHandler.Verify(x => x.Cleanup(It.IsAny<string>()), Times.Exactly(3));
+		}
+
+		[Test]
+		public async Task SyncAsync_When_CheckForUpdates_Enabled_Should_Check()
+		{
+			// SETUP
+			var mocker = new AutoMocker();
+
+			var service = mocker.CreateInstance<SyncService>();
+			var peloton = mocker.GetMock<IPelotonService>();
+			var db = mocker.GetMock<ISyncStatusDb>();
+			var settingsService = mocker.GetMock<ISettingsService>();
+			var ghService = mocker.GetMock<IGitHubService>();
+			
+			var settings = new Settings();
+			settings.App.CheckForUpdates = true;
+			settingsService.Setup(s => s.GetSettingsAsync()).ReturnsAsync(settings);
+
+			var syncStatus = new SyncServiceStatus();
+			db.Setup(x => x.GetSyncStatusAsync()).Returns(Task.FromResult(syncStatus));
+			peloton.Setup(x => x.GetRecentWorkoutsAsync(0)).ReturnsAsync(new List<Workout>());
+
+			ghService.Setup(x => x.GetLatestReleaseAsync())
+				.ReturnsAsync(new P2GLatestRelease())
+				.Verifiable();
+
+			// ACT
+			var response = await service.SyncAsync(0);
+
+			// ASSERT
+			ghService.Verify();
+		}
+
+		[Test]
+		public async Task SyncAsync_When_CheckForUpdates_Disabled_Should_NotCheck()
+		{
+			// SETUP
+			var mocker = new AutoMocker();
+
+			var service = mocker.CreateInstance<SyncService>();
+			var peloton = mocker.GetMock<IPelotonService>();
+			var db = mocker.GetMock<ISyncStatusDb>();
+			var settingsService = mocker.GetMock<ISettingsService>();
+			var ghService = mocker.GetMock<IGitHubService>();
+
+			var settings = new Settings();
+			settings.App.CheckForUpdates = false;
+			settingsService.Setup(s => s.GetSettingsAsync()).ReturnsAsync(settings);
+
+			var syncStatus = new SyncServiceStatus();
+			db.Setup(x => x.GetSyncStatusAsync()).Returns(Task.FromResult(syncStatus));
+			peloton.Setup(x => x.GetRecentWorkoutsAsync(0)).ReturnsAsync(new List<Workout>());
+
+			ghService.Setup(x => x.GetLatestReleaseAsync())
+				.ReturnsAsync(new P2GLatestRelease());
+
+			// ACT
+			var response = await service.SyncAsync(0);
+
+			// ASSERT
+			ghService.Verify(x => x.GetLatestReleaseAsync(), Times.Never);
 		}
 	}
 }
