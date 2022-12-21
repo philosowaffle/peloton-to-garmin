9378876ca045b7a5f8f4d46cb6a30698b5a6619d
Bailey Belvis <baileyb622@gmail.com>
2022-12-21 09:58:26 -0600

    clean up and tests

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -71,32 +71,35 @@ builder.Services.AddSwaggerGen(c =>
 // CACHE
 builder.Services.AddSingleton<IMemoryCache, MemoryCache>();
 
-// SETTINGS
-builder.Services.AddSingleton<ISettingsDb, SettingsDb>();
-builder.Services.AddSingleton<ISettingsService, SettingsService>();
-
-// IO
-builder.Services.AddSingleton<IFileHandling, IOWrapper>();
-
-// PELOTON
-builder.Services.AddSingleton<IPelotonApi, Peloton.ApiClient>();
-builder.Services.AddSingleton<IPelotonService, PelotonService>();
+// CONVERT
+builder.Services.AddSingleton<IConverter, FitConverter>();
+builder.Services.AddSingleton<IConverter, TcxConverter>();
+builder.Services.AddSingleton<IConverter, JsonConverter>();
 
 // GARMIN
 builder.Services.AddSingleton<IGarminUploader, GarminUploader>();
 builder.Services.AddSingleton<IGarminApiClient, Garmin.ApiClient>();
 
+// IO
+builder.Services.AddSingleton<IFileHandling, IOWrapper>();
+
+// MIGRATIONS
+builder.Services.AddSingleton<IDbMigrations, DbMigrations>();
+
+// PELOTON
+builder.Services.AddSingleton<IPelotonApi, Peloton.ApiClient>();
+builder.Services.AddSingleton<IPelotonService, PelotonService>();
+
 // RELEASE CHECKS
 builder.Services.AddGitHubReleaseChecker();
+
+// SETTINGS
+builder.Services.AddSingleton<ISettingsDb, SettingsDb>();
+builder.Services.AddSingleton<ISettingsService, SettingsService>();
 
 // SYNC
 builder.Services.AddSingleton<ISyncStatusDb, SyncStatusDb>();
 builder.Services.AddSingleton<ISyncService, SyncService>();
-
-// CONVERT
-builder.Services.AddSingleton<IConverter, FitConverter>();
-builder.Services.AddSingleton<IConverter, TcxConverter>();
-builder.Services.AddSingleton<IConverter, JsonConverter>();
 
 // USERS
 builder.Services.AddSingleton<IUsersDb, UsersDb>();
@@ -155,48 +158,8 @@ app.MapControllers();
 ///////////////////////////////////////////////////////////
 /// MIGRATIONS
 ///////////////////////////////////////////////////////////
-#pragma warning disable CS0612 // Type or member is obsolete
-var settingsDb = app.Services.GetService<ISettingsDb>();
-var settings = settingsDb!.GetLegacySettings();
-
-var usersDb = app.Services.GetService<IUsersDb>();
-var users = await usersDb!.GetUsersAsync();
-var admin = users.First();
-
-// Migrate to Admin user
-if (settings is object)
-{
-	try
-	{
-		var success = await settingsDb.UpsertSettingsAsync(admin.Id, settings);
-		if (success) await settingsDb.RemoveLegacySettingsAsync();
-		else Log.Error("[MIGRATION] Failed to migrate existing settings to Admin user.");
-
-		var syncStatusDb = app.Services.GetService<ISyncStatusDb>();
-		await syncStatusDb!.DeleteLegacySyncStatusAsync();
-		Log.Information("[MIGRATION] Successfully migrated existing data to new Admin user.");
-	}
-	catch (Exception e)
-	{
-		Log.Error(e, "[MIGRATION] Failed to migrate existing data to Admin user.");
-	}
-}
-
-// Migrate to Encrypted Credentials V1
-settings = await settingsDb!.GetSettingsAsync(admin.Id);
-if (settings.Peloton.EncryptionVersion != EncryptionVersion.V1
-	|| settings.Garmin.EncryptionVersion != EncryptionVersion.V1)
-{
-	try
-	{
-		await settingsDb.UpsertSettingsAsync(admin.Id, settings);
-		Log.Information("[MIGRATION] Successfully encrypted Peloton and Garmin credentials.");
-	} catch (Exception e)
-	{
-		Log.Error(e, "[MIGRATION] Failed to encrypt Peloton and Garmin credentials.");
-	}	
-}
-#pragma warning restore CS0612 // Type or member is obsolete
+var migrationService = app.Services.GetService<IDbMigrations>();
+await migrationService!.PreformMigrations();
 
 ///////////////////////////////////////////////////////////
 /// START
---
+++ src/Common/Database/DbMigrations.cs
@@ -0,0 +1,98 @@
+﻿using Common.Observe;
+using Serilog;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Common.Database;
+
+public interface IDbMigrations
+{
+	public Task PreformMigrations();
+}
+
+public class DbMigrations : IDbMigrations
+{
+	private static readonly ILogger _logger = LogContext.ForClass<DbMigrations>();
+
+	private readonly ISettingsDb _settingsDb;
+	private readonly IUsersDb _usersDb;
+	private readonly ISyncStatusDb _syncStatusDb;
+
+	public DbMigrations(ISettingsDb settingsDb, IUsersDb usersDb, ISyncStatusDb syncStatusDb)
+	{
+		_settingsDb = settingsDb;
+		_usersDb = usersDb;
+		_syncStatusDb = syncStatusDb;
+	}
+
+	public async Task PreformMigrations()
+	{
+		await MigrateToAdminUserAsync();
+		await MigrateToEncryptedCredentialsAsync();
+	}
+
+	public async Task MigrateToAdminUserAsync()
+	{
+		#pragma warning disable CS0612 // Type or member is obsolete
+		var legacySettings = _settingsDb.GetLegacySettings();
+
+		if (legacySettings is null) return;
+
+		_logger.Information("[MIGRATION] Migrating settings to Admin user...");
+
+		var users = await _usersDb.GetUsersAsync();
+		var admin = users.First();
+
+		try
+		{
+			var success = await _settingsDb.UpsertSettingsAsync(admin.Id, legacySettings);
+			if (success)
+			{
+				await _settingsDb.RemoveLegacySettingsAsync();
+				_logger.Information("[MIGRATION] Successfully migrated existing data to new Admin user.");
+			}
+			else
+			{
+				_logger.Error("[MIGRATION] Failed to migrate existing settings to Admin user.");
+			}
+		}
+		catch (Exception e)
+		{
+			_logger.Error(e, "[MIGRATION] Failed to migrate existing data to Admin user.");
+		}
+
+		try
+		{
+			await _syncStatusDb!.DeleteLegacySyncStatusAsync();
+		}
+		catch (Exception e)
+		{
+			_logger.Warning(e, "[MIGRATION Failed to delete LegacySyncStatus.");
+		}
+		#pragma warning restore CS0612 // Type or member is obsolete
+	}
+
+	public async Task MigrateToEncryptedCredentialsAsync()
+	{
+		var admin = (await _usersDb.GetUsersAsync()).First();
+		var settings = await _settingsDb!.GetSettingsAsync(admin.Id);
+
+		if (settings.Peloton.EncryptionVersion == EncryptionVersion.V1
+			&& settings.Garmin.EncryptionVersion == EncryptionVersion.V1)
+			return;
+
+		_logger.Information("[MIGRATION] Encrypting Peloton and Garmin credentials...");
+
+		try
+		{
+			await _settingsDb.UpsertSettingsAsync(admin.Id, settings);
+			_logger.Information("[MIGRATION] Successfully encrypted Peloton and Garmin credentials.");
+		}
+		catch (Exception e)
+		{
+			_logger.Error(e, "[MIGRATION] Failed to encrypt Peloton and Garmin credentials.");
+		}
+
+	}
+}
