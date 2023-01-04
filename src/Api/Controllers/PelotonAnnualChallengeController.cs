fa41fd916dc13f3a8aa05d58ff74d3a4758b62a4
Bailey Belvis <baileyb622@gmail.com>
2023-01-04 17:41:59 -0600

    [406] WebUI - add page to see progress towards Annual Challenge tiers

---
+++ src/Api/Controllers/PelotonAnnualChallengeController.cs
@@ -0,0 +1,37 @@
+﻿using Common.Dto.Api;
+using Microsoft.AspNetCore.Mvc;
+using Peloton;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+[Consumes("application/json")]
+public class PelotonAnnualChallengeController : Controller
+{
+	private readonly IAnnualChallengeService _annualChallengeService;
+
+	public PelotonAnnualChallengeController(IAnnualChallengeService annualChallengeService)
+	{
+		_annualChallengeService = annualChallengeService;
+	}
+
+	/// <summary>
+	/// Fetches a progress summary for the Peloton Annual Challenge.
+	/// </summary>
+	/// <response code="200">Returns the progress summary</response>
+	/// <response code="400">Invalid request values.</response>
+	/// <response code="500">Unhandled exception.</response>
+	[HttpGet]
+	[Route("api/pelotonannualchallenge/progress")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+	public async Task<ActionResult<ProgressGetResponse>> GetProgressSummaryAsync()
+	{
+		var userId = 1;
+		var result = await _annualChallengeService.GetAnnualChallengeProgressAsync(userId);
+
+		return null;
+	}
+}
---
+++ src/Common/Dto/Api/PelotonAnnualChallengeContracts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Common.Dto.Api;
+
+public record ProgressGetResponse
+{
+	public short EarnedMinutes { get; set; }
+	public ICollection<Tier> Tiers { get; set; }
+}
+
+public record Tier
+{
+	public string BadgeUrl { get; set; } // tiers.detailed_badge_image
+	public string Title { get; set; }
+	public short RequiredMinutes { get; set; }
+	public bool HasEarned { get; set; }
+	public bool IsOnTrackToEarndByEndOfYear { get; set; }
+	public bool MinutesBehindPace { get; set; }
+	public bool MinutesAheadOfPace { get; set; }
+}
