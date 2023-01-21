40862ebf72d8c853fcbb7838a722ba89ca5e2f4b
Bailey <baileyb622@gmail.com>
2023-01-21 12:05:48 -0600

    create client UI project and wire up first shared page

---
+++ src/ClientUI/MauiProgram.cs
@@ -0,0 +1,127 @@
+﻿using ClientUI.Data;
+using Common.Http;
+using Common.Observe;
+using Common;
+using Microsoft.Extensions.Logging;
+using Philosowaffle.Capability.ReleaseChecks;
+using Serilog;
+using SharedUI;
+using Common.Stateful;
+using Microsoft.Extensions.Configuration;
+using Havit.Blazor.Components.Web;
+using Common.Service;
+using Common.Database;
+using Microsoft.Extensions.Caching.Memory;
+using Conversion;
+using Garmin;
+using Peloton;
+using Peloton.AnnualChallenge;
+using Sync;
+
+namespace ClientUI;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		///////////////////////////////////////////////////////////
+		/// STATICS
+		///////////////////////////////////////////////////////////
+		Statics.AppType = Constants.ClientUIName;
+		Statics.MetricPrefix = Constants.ClientUIName;
+		Statics.TracingService = Constants.ClientUIName;
+
+		///////////////////////////////////////////////////////////
+		/// HOST
+		///////////////////////////////////////////////////////////
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+
+		var configProvider = builder.Configuration.AddJsonFile(Path.Join(Environment.CurrentDirectory, "configuration.local.json"), optional: true, reloadOnChange: true)
+				.AddEnvironmentVariables(prefix: "P2G_");
+
+		var config = new AppConfiguration();
+		ConfigurationSetup.LoadConfigValues(builder.Configuration, config);
+
+		///////////////////////////////////////////////////////////
+		/// SERVICES
+		///////////////////////////////////////////////////////////
+
+		// API CLIENT
+		builder.Services.AddSingleton<IApiClient, ServiceClient>();
+
+		// CACHE
+		builder.Services.AddSingleton<IMemoryCache, MemoryCache>();
+
+		// CONVERT
+		builder.Services.AddSingleton<IConverter, FitConverter>();
+		builder.Services.AddSingleton<IConverter, TcxConverter>();
+		builder.Services.AddSingleton<IConverter, JsonConverter>();
+
+		// GARMIN
+		builder.Services.AddSingleton<IGarminUploader, GarminUploader>();
+		builder.Services.AddSingleton<IGarminApiClient, Garmin.ApiClient>();
+
+		// IO
+		builder.Services.AddSingleton<IFileHandling, IOWrapper>();
+
+		// HAVIT
+		builder.Services.AddHxServices();
+		builder.Services.AddHxMessenger();
+
+		// MIGRATIONS
+		builder.Services.AddSingleton<IDbMigrations, DbMigrations>();
+
+		// PELOTON
+		builder.Services.AddSingleton<IPelotonApi, Peloton.ApiClient>();
+		builder.Services.AddSingleton<IPelotonService, PelotonService>();
+		builder.Services.AddSingleton<IAnnualChallengeService, AnnualChallengeService>();
+
+		// RELEASE CHECKS
+		builder.Services.AddGitHubReleaseChecker();
+
+		// SETTINGS
+		builder.Services.AddSingleton<ISettingsDb, SettingsDb>();
+		builder.Services.AddSingleton<ISettingsService, SettingsService>();
+
+		// SYNC
+		builder.Services.AddSingleton<ISyncStatusDb, SyncStatusDb>();
+		builder.Services.AddSingleton<ISyncService, SyncService>();
+
+		// USERS
+		builder.Services.AddSingleton<IUsersDb, UsersDb>();
+
+		FlurlConfiguration.Configure(config.Observability, 30);
+		Tracing.EnableWebUITracing(builder.Services, config.Observability.Jaeger);
+
+		Log.Logger = new LoggerConfiguration()
+						.ReadFrom.Configuration(builder.Configuration, sectionName: $"{nameof(Observability)}:Serilog")
+						.Enrich.FromLogContext()
+						.CreateLogger();
+
+		Logging.LogSystemInformation();
+
+		///////////////////////////////////////////////////////////
+		/// APP
+		///////////////////////////////////////////////////////////
+
+		builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+	builder.Logging.AddDebug();
+#endif
+
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		// Setup initial Tracing Source
+		Tracing.Source = new(Statics.TracingService);
+
+		return builder.Build();
+	}
+}
---
+++ src/ClientUI/ServiceClient.cs
@@ -0,0 +1,126 @@
+﻿using Common;
+using Common.Dto;
+using Common.Dto.Api;
+using Common.Service;
+using Core.GitHub;
+using Microsoft.AspNetCore.Http;
+using Philosowaffle.Capability.ReleaseChecks.Model;
+using SharedUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientUI;
+
+public class ServiceClient : IApiClient
+{
+	private readonly IGitHubReleaseCheckService _gitHubService;
+	private readonly ISettingsService _settingsService;
+
+	//public ServiceClient(IGitHubReleaseCheckService gitHubService, ISettingsService settingsService)
+	public ServiceClient(IGitHubReleaseCheckService gitHubService)
+	{
+		_gitHubService = gitHubService;
+		//_settingsService = settingsService;
+	}
+
+	public Task<ProgressGetResponse> GetAnnualProgressAsync()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<PelotonWorkoutsGetResponse> PelotonWorkoutsGetAsync(PelotonWorkoutsGetRequest request)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<PelotonWorkoutsGetAllResponse> PelotonWorkoutsGetAsync(PelotonWorkoutsGetAllRequest request)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Common.App> SettingsAppPostAsync(Common.App appSettings)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<Format> SettingsFormatPostAsync(Format formatSettings)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<SettingsGarminGetResponse> SettingsGarminPostAsync(SettingsGarminPostRequest garminSettings)
+	{
+		throw new NotImplementedException();
+	}
+
+	public async Task<SettingsGetResponse> SettingsGetAsync()
+	{
+		try
+		{
+			// TODO: DB tries to create directory but is not allowed because
+			// we're running in a Win Sys32 dir
+			return new SettingsGetResponse();
+			var settings = await _settingsService.GetSettingsAsync();
+
+			var settingsResponse = new SettingsGetResponse(settings);
+			settingsResponse.Peloton.Password = null;
+			settingsResponse.Garmin.Password = null;
+
+			return settingsResponse;
+		}
+		catch (Exception e)
+		{
+			throw new ApiClientException($"Unexpected error ocurred: {e.Message}", e);
+		}
+	}
+
+	public Task<SettingsPelotonGetResponse> SettingsPelotonPostAsync(SettingsPelotonPostRequest pelotonSettings)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<SyncGetResponse> SyncGetAsync()
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<SyncPostResponse> SyncPostAsync(SyncPostRequest syncPostRequest)
+	{
+		throw new NotImplementedException();
+	}
+
+	public async Task<SystemInfoGetResponse> SystemInfoGetAsync(SystemInfoGetRequest systemInfoGetRequest)
+	{
+		LatestReleaseInformation? versionInformation = null;
+
+		if (systemInfoGetRequest.CheckForUpdate)
+			versionInformation = await _gitHubService.GetLatestReleaseInformationAsync("philosowaffle", "peloton-to-garmin", Constants.AppVersion);
+
+		return new SystemInfoGetResponse()
+		{
+			OperatingSystem = SystemInformation.OS,
+			OperatingSystemVersion = SystemInformation.OSVersion,
+
+			RunTimeVersion = SystemInformation.RunTimeVersion,
+
+			Version = Constants.AppVersion,
+			NewerVersionAvailable = versionInformation?.IsReleaseNewerThanInstalledVersion,
+			LatestVersionInformation = systemInfoGetRequest.CheckForUpdate ? new LatestVersionInformation()
+			{
+				LatestVersion = versionInformation?.LatestVersion,
+				ReleaseDate = versionInformation?.ReleaseDate.ToString(),
+				ReleaseUrl = versionInformation?.ReleaseUrl,
+				Description = versionInformation?.Description
+			} : null,
+
+			GitHub = "https://github.com/philosowaffle/peloton-to-garmin",
+			Documentation = "https://philosowaffle.github.io/peloton-to-garmin/",
+			Forums = "https://github.com/philosowaffle/peloton-to-garmin/discussions",
+			Donate = "https://www.buymeacoffee.com/philosowaffle",
+			Issues = "https://github.com/philosowaffle/peloton-to-garmin/issues"
+		};
+	}
+}
