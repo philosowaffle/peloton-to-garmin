8f19eaa695c301c25efc8b66bd5a93b1165f15ca
Bailey <baileyb622@gmail.com>
2023-01-29 08:29:10 -0600

    [430] fix where error throw when * in workout title

--- src/Common/Constants.cs
+++ src/Common/Constants.cs
@@ -8,6 +8,6 @@
 		public const string ApiName = "p2g_api";
 		public const string WebUIName = "p2g_webui";
 
-		public const string AppVersion = "3.3.1";
+		public const string AppVersion = "3.3.2";
 	}
 }
