9c8eab84d4b4229fffb40b44ad11dd48619196c3
Bailey <baileyb622@gmail.com>
2023-01-29 12:16:51 -0600

    [430] fix where error throw when * in workout title (#431)
    * [430] fix where error throw when * in workout title
    
    * try exclude test

--- src/Common/Helpers/WorkoutHelper.cs
+++ src/Common/Helpers/WorkoutHelper.cs
@@ -1,34 +1,42 @@
 ﻿using Common.Dto.Peloton;
 using System.IO;
 
-namespace Common.Helpers
+namespace Common.Helpers;
+
+public static class WorkoutHelper
 {
-	public static class WorkoutHelper
+	public const char SpaceSeparator = '_';
+
+	private static readonly char[] InvalidFileNameChars = Path.GetInvalidFileNameChars();	
+
+	public static string GetTitle(Workout workout)
 	{
-		public static string GetTitle(Workout workout)
+		var rideTitle = workout.Ride?.Title ?? workout.Id;
+		var instructorName = workout.Ride?.Instructor?.Name;
+
+		if (instructorName is object)
+			instructorName = $" with {instructorName}";
+
+		var title = $"{rideTitle}{instructorName}"
+			.Replace(' ', SpaceSeparator);
+
+		foreach (var c in InvalidFileNameChars)
 		{
-			var rideTitle = workout.Ride?.Title ?? workout.Id;
-			var instructorName = workout.Ride?.Instructor?.Name;
-
-			if (instructorName is object)
-				instructorName = $" with {instructorName}";
-
-			return $"{rideTitle}{instructorName}"
-				.Replace(" ", "_")
-				.Replace("/", "-")
-				.Replace(":", "-");
+			title = title.Replace(c, '-');
 		}
 
-		public static string GetUniqueTitle(Workout workout)
-		{
-			return $"{workout.Id}_{GetTitle(workout)}";
-		}
+		return title;
+	}
 
-		public static string GetWorkoutIdFromFileName(string filePath)
-		{
-			var fileName = Path.GetFileNameWithoutExtension(filePath);
-			var parts = fileName.Split("_");
-			return parts[0];
-		}
+	public static string GetUniqueTitle(Workout workout)
+	{
+		return $"{workout.Id}_{GetTitle(workout)}";
+	}
+
+	public static string GetWorkoutIdFromFileName(string filePath)
+	{
+		var fileName = Path.GetFileNameWithoutExtension(filePath);
+		var parts = fileName.Split("_");
+		return parts[0];
 	}
 }
--- vNextReleaseNotes.md
+++ vNextReleaseNotes.md
@@ -1,16 +1,7 @@
 [![](https://img.shields.io/static/v1?label=Sponsor&message=%E2%9D%A4&logo=GitHub&color=%23fe8e86)](https://github.com/sponsors/philosowaffle) <span class="badge-buymeacoffee"><a href="https://www.buymeacoffee.com/philosowaffle" title="Donate to this project using Buy Me A Coffee"><img src="https://img.shields.io/badge/buy%20me%20a%20coffee-donate-yellow.svg" alt="Buy Me A Coffee donate button" /></a></span>
 ---
 
-## Features
-
-- [#406] WebUI - New page to view more detailed progress towards Annual Challenge Badges
-
 ## Fixes
 
-- [#404] Fixed where outdoor Just walk/Just run could be missing GPS data on Garmin Connect
-- [#411] WebUI - Fixed issue where custom DeviceInfo file path could not be saved
-- [#414] Fixed calories missing on Garmin Connect when provided by Apple Watch
+- [#430] Fixed where error thrown for classes with `*` in the title 
 
-## Changes
-
-- [#402] [housekeeping] Dependency bumps
