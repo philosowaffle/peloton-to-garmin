255a16786bbf5ce454e01b69ea3d39046f8233ea
Bailey <baileyb622@gmail.com>
2023-04-30 08:10:39 -0500

    [479] winui remove metrics/tracing support (#490)

--- src/Common/ObservabilityStartup.cs
+++ src/Common/ObservabilityStartup.cs
@@ -4,19 +4,48 @@ using Common.Stateful;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
+using Serilog.Enrichers.Span;
 using System.IO;
 
 namespace Common;
 
 public static class ObservabilityStartup
 {
-	public static void Configure(IServiceCollection services, ConfigurationManager configManager, AppConfiguration config, bool hardcodeFileLogging = false)
+	public static void ConfigureClientUI(IServiceCollection services, ConfigurationManager configManager, AppConfiguration config)
+	{
+		FlurlConfiguration.Configure(config.Observability);
+		ConfigureLogging(configManager, hardcodeFileLogging:true);
+
+		// Setup initial Tracing Source
+		Tracing.Source = new(Statics.TracingService);
+	}
+
+	public static void ConfigureApi(IServiceCollection services, ConfigurationManager configManager, AppConfiguration config)
+	{
+		FlurlConfiguration.Configure(config.Observability);
+		Tracing.EnableApiTracing(services, config.Observability.Jaeger);
+		ConfigureLogging(configManager);
+
+		// Setup initial Tracing Source
+		Tracing.Source = new(Statics.TracingService);
+	}
+
+	public static void ConfigureWebUI(IServiceCollection services, ConfigurationManager configManager, AppConfiguration config)
 	{
 		FlurlConfiguration.Configure(config.Observability);
 		Tracing.EnableWebUITracing(services, config.Observability.Jaeger);
+		ConfigureLogging(configManager);
 
+		// Setup initial Tracing Source
+		Tracing.Source = new(Statics.TracingService);
+	}
+
+	private static void ConfigureLogging(ConfigurationManager configManager, bool hardcodeFileLogging = false)
+	{
 		var loggingConfig = new LoggerConfiguration()
-						.ReadFrom.Configuration(configManager, sectionName: $"{nameof(Observability)}:Serilog");
+						.ReadFrom.Configuration(configManager, sectionName: $"{nameof(Observability)}:Serilog")
+						.Enrich.WithSpan()
+						.Enrich.FromLogContext();
 
 		if (hardcodeFileLogging)
 			loggingConfig.WriteTo.File(Path.Join(Statics.DefaultOutputDirectory, "log.txt"), rollingInterval: RollingInterval.Day, retainedFileCountLimit: 7);
