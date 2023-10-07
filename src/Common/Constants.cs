d5501b0435e9f96a3bde0f8796a11a4a0bcdd9d6
Bailey <baileyb622@gmail.com>
2023-10-07 11:44:15 -0500

    version bump for 3.6.0 release

--- src/Common/Constants.cs
+++ src/Common/Constants.cs
@@ -7,6 +7,6 @@
 		public const string ConsoleAppName = "p2g_console";
 		public const string ApiName = "p2g_api";
 		public const string WebUIName = "p2g_webui";
-		public const string AppVersion = "3.6.0-rc";
+		public const string AppVersion = "3.6.0";
 	}
 }
