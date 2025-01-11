202aad3b3534dd2905a750e828e29e9d41992549
Bailey <baileyb622@gmail.com>
2025-01-11 08:58:08 -0600

    prevent saving illegal characters in password

--- src/Api.Service/SettingsUpdaterService.cs
+++ src/Api.Service/SettingsUpdaterService.cs
@@ -87,6 +87,14 @@ public class SettingsUpdaterService : ISettingsUpdaterService
 			return result;
 		}
 
+		if (!string.IsNullOrWhiteSpace(updatedGarminSettings.Password)
+			&& updatedGarminSettings.Password.Contains('\\'))
+		{
+			result.Successful = false;
+			result.Error = new ServiceError() { Message = "P2G does not support the `\\` character in passwords." };
+			return result;
+		}
+
 		var settings = await _settingsService.GetSettingsAsync();
 
 		if (settings.Garmin.Password != updatedGarminSettings.Password
